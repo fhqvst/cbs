1660fb947de70924ae23814cd606dffc81790a1d
Filip Hallqvist <hej@filiphallqvist.se>
2015-08-03 00:55:34 +0200

    Highstocks styling

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -9,7 +9,7 @@ var React = require('react');
 
     function initialize() {
 
-        $('.block__toggle').click(function () {
+        $('.block__header').click(function () {
             $(this).parents('.block').toggleClass('is-minimized');
             $(this).find('i').toggleClass('ion-ios-minus-empty');
             $(this).find('i').toggleClass('ion-ios-plus-empty');
@@ -49,7 +49,110 @@ var React = require('react');
             });
         });
 
+        var colors = {
+            red: '#FB0E29',
+            green: '#38BBA5',
+            blue: '#38a5bb',
+            black: '#333',
+            white: '#fff',
+            gray: '#ccc',
+            offWhite: '#eee',
+            orange: '#FF6E4C'
+        };
+
         $.getJSON('http://www.highcharts.com/samples/data/jsonp.php?a=e&filename=aapl-ohlc.json&callback=?', function (data) {
+
+            Highcharts.theme = {
+                title: {
+                    style: {
+                        color: '#333',
+                        font: 'bold 16px DIN Pro, Roboto, Helvetica, sans-serif'
+                    }
+                },
+                subtitle: {
+                    style: {
+                        color: '#333',
+                        font: 'bold 12px serif'
+                    }
+                },
+
+                plotOptions: {
+                    candlestick: {
+                        color: colors.orange,
+                        upColor: colors.green,
+                        lineColor: colors.orange,
+                        upLineColor: colors.green,
+                        states: {
+                            hover: {
+                                lineWidth: 1
+                            }
+                        }
+                    }
+                },
+
+                tooltip: {
+                    backgroundColor: 'rgba(255,255,255,0.75)',
+                    borderColor: 'rgba(0,0,0,0.25)',
+                    borderWidth: 1,
+                    borderRadius: 0,
+                    shape: 'square',
+                    shadow: false
+                },
+
+                rangeSelector: {
+                    buttonTheme: { // styles for the buttons
+                        fill: 'none',
+                        stroke: 'none',
+                        'stroke-width': 0,
+                        style: {
+                            color: colors.gray,
+                            background: 'transparent',
+                            textTransform: 'uppercase',
+                            fontFamily: 'DIN Pro',
+                            letterSpacing: '0.05em'
+                        },
+                        states: {
+                            hover: {
+                                fill: 'none',
+                                style: {
+                                    background: 'transparent',
+                                    color: colors.black
+                                }
+                            },
+                            select: {
+                                fill: 'none',
+                                style: {
+                                    color: colors.black
+                                }
+                            }
+                        }
+                    },
+                    inputStyle: {
+                        color: '#039',
+                        fontWeight: 'bold'
+                    },
+                    labelStyle: {
+                        left: 50
+                    },
+                    selected: 1
+                },
+
+                legend: {
+                    itemStyle: {
+                        font: '9pt Calibri, Roboto, sans-serif',
+                        color: 'black'
+                    },
+                    itemHoverStyle: {
+                        color: '#38BBA5'
+                    }
+                },
+
+                navigator: {
+                    outlineColor: colors.gray
+                }
+            };
+
+            Highcharts.setOptions(Highcharts.theme);
 
             // create the chart
             $('.instrument__chart__inner').highcharts('StockChart', {
@@ -57,11 +160,6 @@ var React = require('react');
                 rangeSelector: {
                     selected: 1
                 },
-
-                title: {
-                    text: 'AAPL Stock Price'
-                },
-
                 series: [{
                     type: 'candlestick',
                     name: 'AAPL Stock Price',
@@ -71,7 +169,31 @@ var React = require('react');
                         [1] // allowed multiples
                         ], ['month', [1, 2, 3, 4, 6]]]
                     }
-                }]
+                }],
+                xAxis: {
+                    gridLineColor: colors.offWhite,
+                    gridLineWidth: 1
+                },
+                yAxis: {
+                    gridLineColor: colors.offWhite,
+                    gridLineWidth: 1
+                },
+
+                credits: {
+                    enabled: false
+                },
+                chart: {
+                    marginTop: 0,
+                    marginLeft: 0,
+                    marginRight: 0,
+                    marginBottom: 25,
+                    spacingRight: 25,
+                    spacingBottom: 0
+                },
+                scrollbar: {
+                    enabled: false
+                }
+
             });
         });
 
