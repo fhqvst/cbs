14479ff0847216cd0b69e88096430eb6a137c0ff
Filip Hallqvist <filip@raket.nu>
2015-08-19 17:02:33 +0200

    Added ticksize field to instruments table

--- database/migrations/2015_08_04_203558_create_instruments_table.php
+++ database/migrations/2015_08_04_203558_create_instruments_table.php
@@ -18,6 +18,7 @@ class CreateInstrumentsTable extends Migration
             $table->increments('id');
             $table->string('symbol');
             $table->text('name');
+            $table->decimal('ticksize', 19, 4);
             $table->integer('nordnet_id')->default(0);
             $table->integer('listing_id')->unsigned();
             $table->timestamps();
